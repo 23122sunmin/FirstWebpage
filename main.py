6916c7dfc8f775df40170e04acaf3883ed2ef78b
23122sunmin <23122@dongdaemun.ms.kr>
2024-09-09 15:50:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title("음식 선호도 조사")
+st.title("버거 선호도 조사")
 name=st.text_input("이름을 입력해주세요!")
 menu=st.selectbox("좋아하는 버거 브랜드를 선택해주세요!",['맥도날드','롯데리아','버거킹','kfc','맘스터치'])
 if st.button("제출"):
