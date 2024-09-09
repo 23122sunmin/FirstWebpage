0cb8992dee43fd6cd3a865bde4fce815fa96202c
23122sunmin <23122@dongdaemun.ms.kr>
2024-09-09 15:49:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
-st.title("my first web servixe!!")
+st.title("음식 선호도 조사")
 name=st.text_input("이름을 입력해주세요!")
-menu=st.selectbox("좋아하는 음식을 선택해주세요!",['셱셱','베라','그릭요거트','파스타'])
+menu=st.selectbox("좋아하는 음식을 선택해주세요!",['맥도날드','롯데리아','버거킹','kfc','맘스터치'])
 if st.button("제출"):
     st.write(name+"님! 당신이 좋아하는 음식은 "+menu+"이군요! 저도 좋아요!")
