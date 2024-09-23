446cd3f29e26574896bf4ba7e7ce1a6ec907a2a2
23122sunmin <23122@dongdaemun.ms.kr>
2024-09-23 15:32:09 +0900

    Update 02-text.py

--- pages/02-text.py
+++ pages/02-text.py
@@ -4,6 +4,6 @@ st.title('FirstWebpage')
 
 st.title('스마일 :sunglasses:')
 
-st.header('아이디어가 안나온다 그아아아아아악 :sparkles:')
+st.header('이거 오ㅓㅏㄴ전 럭키비키잖아? :sparkles::sunglasses::sparkles:')
 
 st.subheader('믕')
