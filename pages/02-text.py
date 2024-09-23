a9370ceafd58db5aa7206b4b120a18500838c1fb
23122sunmin <23122@dongdaemun.ms.kr>
2024-09-23 15:38:58 +0900

    Update 02-text.py

--- pages/02-text.py
+++ pages/02-text.py
@@ -7,3 +7,13 @@ st.title('쌈@뽕한 공지사이트 :sunglasses:')
 st.header('이거 오ㅓㅏㄴ전 럭키비키잖아? :sparkles::sunglasses::sparkles:')
 
 st.subheader(':thinking:')
+
+st.caption('캡션은 짧은 설명을 추가하는 것!')
+
+sample_code = ...
+def function():
+  print('hello world')
+...
+st.code(sample_code, language="python")
+
+st.text('일반적인 텍스트를 입력해보았슴')
