bb1e5306105703aa2dc3442b6218f159cba22786
23122sunmin <23122@dongdaemun.ms.kr>
2024-09-23 15:29:58 +0900

    Update 02-text.py

--- pages/02-text.py
+++ pages/02-text.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-st.title('ㅇㅅㅇ')
+st.title('FirstWebpage')
 
 st.title('스마일 :sunglasses:')
 
-st.header('^^ :sparkles:')
+st.header('아이디어가 안나온다 그아아아아아악 :sparkles:')
 
-st.subheader('ㅅㅇㅅ')
+st.subheader('믕')
