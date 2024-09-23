c6fe7b398ea1e7dced1429548e829d0c0b9ecd67
23122sunmin <23122@dongdaemun.ms.kr>
2024-09-23 15:40:02 +0900

    Update 02-text.py

--- pages/02-text.py
+++ pages/02-text.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('2-7')
 
-st.title('쌈@뽕한 공지사이트 :sunglasses:')
+st.title('쌈@뽕한 첫 사이트 :sunglasses:')
 
 st.header('이거 오ㅓㅏㄴ전 럭키비키잖아? :sparkles::sunglasses::sparkles:')
 
