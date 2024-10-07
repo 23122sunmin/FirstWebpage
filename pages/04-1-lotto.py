335a13c33b37b31cd58770f3a6da7b6925ed3702
23122sunmin <23122@dongdaemun.ms.kr>
2024-10-07 15:40:41 +0900

    Update 04-1-lotto.py

--- pages/04-1-lotto.py
+++ pages/04-1-lotto.py
@@ -32,20 +32,19 @@ button = st.button('로또 생성')
 if button:
     st.subheader("정답 로또 번호 (숨김 처리)")
     st.write(hidden_lotto)  # Debugging purposes, this line can be hidden if you don't want to reveal the numbers.
-    
+
     for i in range(1, 6):
         user_lotto = generate_lotto()
         highlighted_lotto = []
 
-        # 자리와 숫자가 모두 일치할 때 노란색으로 표시
+        # 자리와 숫자가 모두 일치할 때 노란색으로 표시 (HTML을 사용)
         for j in range(6):
             if user_lotto[j] == hidden_lotto[j]:
-                highlighted_lotto.append(f':yellow[{user_lotto[j]}]')  # 노란색으로 표시
+                highlighted_lotto.append(f'<span style="color:yellow">{user_lotto[j]}</span>')  # 노란색으로 표시
             else:
                 highlighted_lotto.append(str(user_lotto[j]))
 
-        # 로또 번호 출력
-        st.subheader(f'{i}. 행운의 번호: {" ".join(highlighted_lotto)}')
+        # 로또 번호 출력 (HTML 스타일링 허용)
+        st.markdown(f'{i}. 행운의 번호: {" ".join(highlighted_lotto)}', unsafe_allow_html=True)
     
-    st.write(f"생성된 시각: {datetime.datetime.now().strftime('%y-%m-%d %H:%M')}")
-
+    st.write(f"생성된 시각: {datetime.datetime.now().strftime("%y-%m-%d %H:%M")}')
