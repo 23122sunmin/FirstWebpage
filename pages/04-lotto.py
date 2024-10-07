062c10c05e186fe6b810e9f8c911586dc052532d
23122sunmin <23122@dongdaemun.ms.kr>
2024-10-07 15:27:54 +0900

    Update 04-lotto.py

--- pages/04-lotto.py
+++ pages/04-lotto.py
@@ -21,4 +21,4 @@ button=st.button('로또 생성')
 if button:
       for i in range (1, 6):
             st.subheader(f'{i}. 행운의 번호: :green[{generate_lotto()}]')
-      st.write(f"생성된 시각: {datetime.datetime.now().strftime('%y-%m-%d %H:%M:@S')}")
+      st.write(f"생성된 시각: {datetime.datetime.now().strftime('%y-%m-%d %H:%M')}")
